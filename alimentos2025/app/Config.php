3ea2bf8b0d51439b5852e6fdfc399924d66b851d
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-11-27 12:38:32 +0100

    Configurado archivo de config.php para la conexion a la BBDD

--- alimentos2025/app/Config.php
+++ alimentos2025/app/Config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_hostname = "mysql-sergiocd.alwaysdata.net";
+     static public $mvc_bd_nombre   = "sergiocd_alimentos32025";
+     static public $mvc_bd_usuario  = "sergiocd";
+     static public $mvc_bd_clave    = "SS!4n*Rp@6JZP#_";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
