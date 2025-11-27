76e37548e8c557cb42044772a68d440e43f6e33b
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-11-27 12:27:26 +0100

    añadido estructura al proyecto

---
+++ sql/script.sql
@@ -0,0 +1,12 @@
+/*Script de instalacion para la base de datos de alimentos*/
+
+CREATE TABLE `alimentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `energia` decimal(10,0) NOT NULL,
+  `proteina` decimal(10,0) NOT NULL,
+  `hidratocarbono` decimal(10,0) NOT NULL,
+  `fibra` decimal(10,0) NOT NULL,
+  `grasatotal` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
